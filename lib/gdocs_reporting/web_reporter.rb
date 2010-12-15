08a841b328b634d06a9ab9edbc93b6cf8145622d
Noah Paessel <knowuh@gmail.com>
2010-12-15 17:17:06 -0500

    Ask input needed conversion to integers.

--- lib/gdocs_reporting/web_reporter.rb
+++ lib/gdocs_reporting/web_reporter.rb
@@ -2,19 +2,21 @@ require 'net/http'
 require 'benchmark'
 module GdocsReporting
   class WebReporter < SheetReporter
-    attr_accessor :page_name, :output_rows, :input_row, :poll_interval
+    attr_accessor :page_name, :output_rows, :input_column, :poll_interval
 
     def initialize
       super
-      self.sheet = ask("Sheet name: ") { |q| q.default = "Automated Tests" }
-      self.page  = ask("Page name:  ") { |q| q.default = "Server response Times" }
-      self.input_row    = ask("       Host (input) row: ") { |q| q.default = 2 }
-      status_row        = ask("       Web response row: ") { |q| q.default = 5 }
-      request_time_row  = ask("  Web response time row: ") { |q| q.default = 6 }
-      timestamp_row     = ask("          Timestamp row: ") { |q| q.default = 7 }
+      self.sheet        = ask("              Sheet name: ") { |q| q.default = "Automated Tests" }
+      self.page         = ask("               Page name: ") { |q| q.default = "Server response Times" }
+      self.input_column = ask("        Host (input) row: ") { |q| q.default = 2 }
+      status_row        = ask("        Web response row: ") { |q| q.default = 5 }
+      request_time_row  = ask("   Web response time row: ") { |q| q.default = 6 }
+      timestamp_row     = ask("           Timestamp row: ") { |q| q.default = 7 }
+      self.poll_interval = ask("Poll Interval (seconds): ") { |q| q.default = 30 }
 
-      self.poll_interval= ask("Poll Interval (seconds): ") { |q| q.default = 120 }
-      self.output_rows = [status_row,request_time_row,timestamp_row]
+      self.poll_interval = self.poll_interval.to_i
+      self.input_column  = self.input_column.to_i
+      self.output_rows = [status_row.to_i,request_time_row.to_i,timestamp_row.to_i]
     end
     
     def check_host(url_string)
@@ -39,7 +41,7 @@ module GdocsReporting
     end
 
     def check_hosts
-      update_rows(self.input_row,self.output_rows) do |input,outputs|
+      update_rows(self.input_column,self.output_rows) do |input,outputs|
         if ( input =~ /\.org/ )
           input = "http://#{input}" unless (input =~ /http/) 
           input = "#{input}/" unless (input =~ /\/$/)
@@ -47,19 +49,22 @@ module GdocsReporting
           outputs[0] = results
           outputs[1] = time
           outputs[2] = Time.now.strftime("%D - %I:%M:%S %p")
+        else
+          puts "WARNING: didn't find well formated host input on #{self.page.title}, column #{self.input_column}, found: #{input}"
         end
       end
     end
     
-    def deamonize(_interval=120)
+    def deamonize(_interval=self.poll_interval)
       error_count = 0
       while (true)
         begin
+          self.page.reload  # check for changes
           self.check_hosts
         rescue
           error_count = error_count + 1;
           delay = error_count * error_count * _interval
-          puts "Error (#{error_count}): $!"
+          puts "Error (#{error_count}): #{$!}"
           sleep (delay)
         else
           error_count = 0
